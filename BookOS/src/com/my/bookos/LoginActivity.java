cc80b87624dbf5141d30cda1d6befd49f1992001
ruhaly <han_lru@163.com>
2014-06-03 10:57:24 +0800

    Signed-off-by: ruhaly <han_lru@163.com>

--- BookOS/src/com/my/bookos/LoginActivity.java
+++ BookOS/src/com/my/bookos/LoginActivity.java
@@ -1,28 +1,33 @@
 package com.my.bookos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.changhong.sdk.activity.SuperActivity;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
-public class LoginActivity extends SuperActivity
-{
-    
-    @Override
-    public void initData()
-    {
-        
-    }
-    
-    @Override
-    public void initLayout(Bundle paramBundle)
-    {
-        setContentView(R.layout.login_layout);
-    }
-    
-    @Override
-    public void clearData()
-    {
-        
-    }
-    
+public class LoginActivity extends SuperActivity {
+
+	@Override
+	public void initData() {
+
+	}
+
+	@Override
+	public void initLayout(Bundle paramBundle) {
+		setContentView(R.layout.login_layout);
+		ViewUtils.inject(this);
+	}
+
+	@Override
+	public void clearData() {
+
+	}
+
+	@OnClick(R.id.btnLogin)
+	public void btnLoginClick(View view) {
+		startActivity(new Intent(getBaseContext(), MainActivity.class));
+	}
 }
