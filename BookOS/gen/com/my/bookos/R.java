3dac1c5a9ab20d0952d7fc9e49bedfc2bd36c215
ruhaly <han_lru@163.com>
2014-06-04 21:19:41 +0800

    SourceTree提交

--- BookOS/gen/com/my/bookos/R.java
+++ BookOS/gen/com/my/bookos/R.java
@@ -147,23 +147,45 @@ public final class R {
     }
     public static final class id {
         public static final int btnLogin=0x7f080003;
-        public static final int container=0x7f080005;
+        public static final int button1=0x7f08000d;
+        public static final int button2=0x7f08000f;
+        public static final int button3=0x7f08000c;
+        public static final int container=0x7f080010;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080002;
+        public static final int editText3=0x7f080009;
+        public static final int editText4=0x7f08000b;
         public static final int frameLogin=0x7f080000;
         public static final int imageView1=0x7f080004;
+        public static final int scrollView1=0x7f080005;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f08000a;
+        public static final int textView5=0x7f08000e;
     }
     public static final class layout {
         public static final int login_layout=0x7f030000;
-        public static final int startloading_layout=0x7f030001;
+        public static final int main_layout=0x7f030001;
+        public static final int startloading_layout=0x7f030002;
     }
     public static final class string {
         public static final int account=0x7f060001;
-        public static final int app_name=0x7f060000;
+        public static final int address=0x7f06000d;
+        public static final int appName=0x7f060000;
+        public static final int bookid=0x7f060006;
         public static final int hint_input_account=0x7f060004;
         public static final int hint_input_pwd=0x7f060005;
+        public static final int last=0x7f06000a;
         public static final int login=0x7f060003;
+        public static final int mobile=0x7f060009;
+        public static final int next=0x7f06000b;
         public static final int pwd=0x7f060002;
+        public static final int search=0x7f06000c;
+        public static final int usemonth=0x7f06000e;
+        public static final int usemonth1=0x7f06000f;
+        public static final int usercode=0x7f060008;
+        public static final int username=0x7f060007;
     }
     public static final class style {
         /** 
